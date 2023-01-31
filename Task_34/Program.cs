44373ba0530efc1bed07b4b158e87a83cd11d0a9
William-J-Butcher <William.J.Billy.Butcher@yandex.ru>
2023-02-01 01:37:50 +0300

    Доработал задачу № 34

---
+++ Task_34/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 34: 
+//Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, 
+//которая покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+int[] array = new int[10];
+int count = 0;
+Console.Write("[ ");
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(100, 1000);
+    Console.Write($"{array[i]}, "); 
+    if(array[i] % 2 == 0) count ++;
+}
+Console.Write("\b\b ] -> ");
+Console.Write($"{count}");
